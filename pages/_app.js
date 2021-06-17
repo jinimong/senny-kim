9c6146498f9ef4db84e5b37b1f00d013c70b19af
jwkim <phoongdeng@gmail.com>
2021-06-18 01:16:22 +0900

    Prevent drag and right mouse click

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div
+      onContextMenu={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
