77036d096ba53ec53acc4288f7278863396799d5
jwkim <phoongdeng@gmail.com>
2021-06-22 18:04:37 +0900

    Add SEO

---
+++ config/next-seo.config.js
@@ -0,0 +1,18 @@
+export default {
+  openGraph: {
+    type: 'website',
+    url: 'https://senny-kim.vercel.app/',
+    title: 'hello world',
+    site_name: 'SiteName',
+    images: [
+      {
+        url: 'https://lh3.googleusercontent.com/proxy/jZDZWeOq6h6X0DkCbPKOWWA-MVIfn9jzqda57y7ycKaQfveh7VvLOBpQoYu0SfLi-3vwTcco8HvCaGxroNnhe51vtotfYDHEgC-Rqhu0hd7l9kSpX0ApdehaOl6rAN_Ev7BYFlQcUuYKUHr3t_YSFUo7iJvMc_JP3BgxfT0Dk-PRvZV9sUCMfBovP46H078ta-Evl_Fnkg90Da6tbMFYzm0rip6FvyOmjk-Uwj5U5NHuI6aQwE4Uk2W-vWXN8vLX_r2f0ukkTebGp5Ryc1vABJkz86pJ_47bdV5LsKRwGOi9KyHNWF5mAWSzMdbBhWn1BsM0ZvWbAnIzQzwjIrllFTUfIpYNK0rcuM9yaTI',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { useCallback } from 'react';
+import { DefaultSeo } from 'next-seo';
 import '../styles/globals.css';
+import SEO from '../config/next-seo.config';
 
 function MyApp({ Component, pageProps }) {
   const prevent = useCallback((e) => {
@@ -7,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <div onContextMenu={prevent} onDragStart={prevent}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </div>
   );
