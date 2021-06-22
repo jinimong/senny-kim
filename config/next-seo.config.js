283d4e60efd22d2deff266f449292726dbedb0f5
jwkim <phoongdeng@gmail.com>
2021-06-22 18:28:38 +0900

    Edit seo config

--- config/next-seo.config.js
+++ config/next-seo.config.js
@@ -2,11 +2,12 @@ export default {
   openGraph: {
     type: 'website',
     url: 'https://senny-kim.vercel.app/',
-    title: 'hello world',
-    site_name: 'SiteName',
+    title: 'KIM SEHYUN',
+    description: 'portfolio',
+    site_name: `Kim Sehyun's Portfolio`,
     images: [
       {
-        url: 'https://lh3.googleusercontent.com/proxy/jZDZWeOq6h6X0DkCbPKOWWA-MVIfn9jzqda57y7ycKaQfveh7VvLOBpQoYu0SfLi-3vwTcco8HvCaGxroNnhe51vtotfYDHEgC-Rqhu0hd7l9kSpX0ApdehaOl6rAN_Ev7BYFlQcUuYKUHr3t_YSFUo7iJvMc_JP3BgxfT0Dk-PRvZV9sUCMfBovP46H078ta-Evl_Fnkg90Da6tbMFYzm0rip6FvyOmjk-Uwj5U5NHuI6aQwE4Uk2W-vWXN8vLX_r2f0ukkTebGp5Ryc1vABJkz86pJ_47bdV5LsKRwGOi9KyHNWF5mAWSzMdbBhWn1BsM0ZvWbAnIzQzwjIrllFTUfIpYNK0rcuM9yaTI',
+        url: 'https://pbs.twimg.com/media/DlODs85VsAAZXyd.jpg',
       },
     ],
   },
