4d3d00475aeebeb142ea8c0c4810f6a3ff09908f
jwkim <phoongdeng@gmail.com>
2021-06-23 23:01:50 +0900

    Improve Navigation

--- components/atoms/Navigation.js
+++ components/atoms/Navigation.js
@@ -1,17 +1,28 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import styled from 'styled-components';
+import { flexCenterStyle } from '../../styles/common';
 
 const Container = styled.nav`
-  display: flex;
-  align-items: center;
   position: fixed;
-  top: 10%;
-  left: 118px;
+  width: 100vw;
+  height: 2rem;
+  top: 5%;
   z-index: 2;
+  color: ${({ color }) => color};
+  ${flexCenterStyle}
+`;
+
+const Logo = styled.div`
+  position: absolute;
+  top: 0;
+  left: 5%;
   backdrop-filter: blur(3px);
   -webkit-backdrop-filter: blur(3px);
   cursor: pointer;
+  ${flexCenterStyle}
+
   &:hover {
     opacity: 0.6;
   }
@@ -27,24 +38,56 @@ const Container = styled.nav`
     letter-spacing: 0.1em;
     white-space: pre-wrap;
     text-align: left;
-    color: ${({ color }) => color};
   }
 `;
 
+const Links = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .active {
+    opacity: 0.5;
+  }
+`;
+
+const ActiveLink = ({ children, href }) => {
+  const { pathname } = useRouter();
+  return (
+    <Link href={href}>
+      <a className={pathname === href ? 'active' : ''}>{children}</a>
+    </Link>
+  );
+};
+
 const Navigation = ({ color }) => (
-  <Link href="/portfolios/langsup">
-    <Container color={color}>
-      <Image
-        src="/assets/logo.png"
-        width={32}
-        height={32}
-        alt="logo"
-        blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
-        placeholder="blur"
-      />
-      <div className="name">{`KIM\nSEHYUN`}</div>
-    </Container>
-  </Link>
+  <Container color={color}>
+    <Links>
+      <ActiveLink href="/aboutme">ABOUT ME</ActiveLink>
+      <span>|</span>
+      <ActiveLink href="/portfolios/langsup">langsup</ActiveLink>
+      <span>|</span>
+      <ActiveLink href="/portfolios/music-app">Music App</ActiveLink>
+      <span>|</span>
+      <ActiveLink href="/portfolios/concerts">Concert</ActiveLink>
+    </Links>
+    <Logo>
+      <Link href="/portfolios/langsup">
+        <>
+          <Image
+            src="/assets/logo.png"
+            width={32}
+            height={32}
+            alt="logo"
+            blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
+            placeholder="blur"
+          />
+          <div className="name">{`KIM\nSEHYUN`}</div>
+        </>
+      </Link>
+    </Logo>
+  </Container>
 );
 
 export default Navigation;
